41f8c724cc7b30d327f6cda9401920634ecc0435
Дмитрий Колб <dm.kolb@gmail.com>
2022-09-06 17:42:39 +0000

    Update tasks/task.py

--- tasks/task.py
+++ tasks/task.py
@@ -4,5 +4,16 @@ ListType = List[Union[int, str]]
 
 
 def get_fizzbuzz_list(n: int) -> ListType:
-    # TODO: Add your code here
-    return []
+    res = []
+    for x in range(1, n + 1):
+        y = ""
+        if x % 3 == 0:
+            y = "Fizz"
+        if x % 5 == 0:
+            y += "Buzz"
+        if y:
+            res.append(y)
+        else:
+            res.append(x)
+
+    return res
