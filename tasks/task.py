e0666171057612202c1910c7ee1fcaebc78e0f30
Gruzin <gruzd.roman@inbox.ru>
2023-03-13 16:12:45 +0300

    Commit with a solution

--- tasks/task.py
+++ tasks/task.py
@@ -5,4 +5,15 @@ ListType = List[Union[int, str]]
 
 def get_fizzbuzz_list(n: int) -> ListType:
     # TODO: Add your code here
-    return []
+    new_list = []
+    for n in range(1, n):
+        new_n = n
+        if n % 3 == 0:
+            new_n = 'Fizz'
+        if n % 5 == 0:
+            new_n = 'buzz'
+        if n % 3 == 0 and n % 5 == 0:
+            new_n = 'FizzBuzz'
+        new_list.append(new_n)
+    return new_list
+
